d38499ad8ae4bcb6d58ca0cf98975de797bde93a
kristina <sikiritskaya@gmail.com>
2021-07-26 23:32:05 +0300

    add filtr

--- js/script.js
+++ js/script.js
@@ -1,14 +1,58 @@
 var burger = document.querySelector('#burger');
 var menuList = document.querySelector('.menu');
-var menuPlus = document.querySelector('.plus');
+
 var menuMail = document.querySelector('.envelope');
 var menuCall = document.querySelector('.call');
+var btnClose = document.querySelector('#close'); 
 
 function openBurger(){
     menuList.classList.add('show');
-    menuPlus.classList.add('show');
-    menuMail.classList.add('show');
-    menuCall.classList.add('show');
+
+    menuMail.classList.add('envelope_show');
+    menuCall.classList.add('call_show'); 
     burger.style.display = 'none';
+    btnClose.style.display = 'block';
 }
+function closeBurger(){
+    menuList.classList.remove('show');
+    menuMail.classList.remove('envelope_show');
+    menuCall.classList.remove('call_show'); 
+    burger.style.display = 'block';
+    btnClose.style.display = 'none';
+}
+
 burger.onclick = openBurger;
+btnClose.onclick = closeBurger;
+
+
+var filtr = document.querySelector('.filtr_form');
+var filtrForm = document.querySelector('.filtr');
+var btnFiltr = document.querySelector('#close_form');
+
+function openFiltr(){
+    filtrForm.style.display = 'block';
+    filtr.style.display = "none";
+    btnFiltr.style.display ="block";
+}
+
+function closeFiltr(){
+    filtrForm.style.display = 'none';
+    filtr.style.display = "block";
+    btnFiltr.style.display ="none";
+}
+ 
+filtr.onclick = openFiltr;
+btnFiltr.onclick = closeFiltr;
+
+
+function outputUpdate(pow) {
+ 
+    document.querySelector('#meaning').value = pow;
+   
+}
+
+/* function changeColor() {
+    var change = document.querySelector('#power');
+    val = change.val();
+    change.css({'background':'-webkit-linear-gradient(left, $color_orange 0%, $color_orange +val+, #EAEAEA +val+, #EAEAEA 100%)'});
+} */
